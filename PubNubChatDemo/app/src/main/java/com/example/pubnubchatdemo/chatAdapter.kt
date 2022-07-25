a473c26efef0712c57f50295429c5cd3504159ff
kelisAndroid <kelish.iqlance@gmail.com>
2022-07-25 18:36:01 +0530

    Complete using 4 Different Layouts

--- PubNubChatDemo/app/src/main/java/com/example/pubnubchatdemo/chatAdapter.kt
+++ PubNubChatDemo/app/src/main/java/com/example/pubnubchatdemo/chatAdapter.kt
@@ -13,29 +13,43 @@ class chatAdapter(var array: ArrayList<ChatMessages>) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     //    var MSG = 0
 //    var IMG = 1
-    var SENDER = 0
-    var RECIEVER = 1
+    var SENDER_IMG = 0
+    var RECIEVER_IMG = 1
+    var SENDER_MSG = 2
+    var RECIEVER_MSG = 3
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): RecyclerView.ViewHolder {
         return when (viewType) {
-            SENDER -> {
+            SENDER_IMG -> {
                 return MessageViewHolder1(
                     LayoutInflater.from(parent.context)
-                        .inflate(R.layout.chat_row_layout_sender_msg, parent, false)
+                        .inflate(R.layout.chat_row_layout_sender_img, parent, false)
                 )
             }
-            RECIEVER -> {
+            RECIEVER_IMG -> {
                 MessageViewHolder2(
                     LayoutInflater.from(parent.context)
-                        .inflate(R.layout.chat_row_layout_reciever_msg, parent, false)
+                        .inflate(R.layout.chat_row_layout_reciever_img, parent, false)
+                )
+            }
+            SENDER_MSG -> {
+                return MessageViewHolder3(
+                    LayoutInflater.from(parent.context)
+                        .inflate(R.layout.chat_row_layout_sender_msg_2, parent, false)
+                )
+            }
+            RECIEVER_MSG -> {
+                MessageViewHolder4(
+                    LayoutInflater.from(parent.context)
+                        .inflate(R.layout.chat_row_layout_reciever_msg_2, parent, false)
                 )
             }
             else -> {
                 return MessageViewHolder1(
                     LayoutInflater.from(parent.context)
-                        .inflate(R.layout.chat_row_layout_sender_msg, parent, false)
+                        .inflate(R.layout.chat_row_layout_sender_msg_2, parent, false)
                 )
             }
         }
@@ -45,6 +59,8 @@ class chatAdapter(var array: ArrayList<ChatMessages>) :
         when (holder) {
             is MessageViewHolder1 -> holder.bind(array[position], position)
             is MessageViewHolder2 -> holder.bind(array[position], position)
+            is MessageViewHolder3 -> holder.bind(array[position], position)
+            is MessageViewHolder4 -> holder.bind(array[position], position)
         }
 
     }
@@ -54,19 +70,12 @@ class chatAdapter(var array: ArrayList<ChatMessages>) :
     }
 
     inner class MessageViewHolder1(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var txtSendMsg: TextView = itemView.findViewById(R.id.txtSendMsg)
         var imgRight: ImageView = itemView.findViewById(R.id.imgRight)
 
-
         fun bind(data: ChatMessages, position: Int) {
-            txtSendMsg.text = data.msg
-            imgRight.LoadImg(data.url.toString())
+
             if (data.is_image == true) {
-                imgRight.visibility = View.VISIBLE
-                txtSendMsg.visibility = View.GONE
-            } else {
-                imgRight.visibility = View.GONE
-                txtSendMsg.visibility = View.VISIBLE
+                imgRight.LoadImg(data.url.toString())
             }
 
         }
@@ -74,31 +83,47 @@ class chatAdapter(var array: ArrayList<ChatMessages>) :
     }
 
     inner class MessageViewHolder2(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var txtRecieveMsg: TextView = itemView.findViewById(R.id.txtRecieveMsg)
         var imgLeft: ImageView = itemView.findViewById(R.id.imgLeft)
 
         fun bind(data: ChatMessages, position: Int) {
-            imgLeft.LoadImg(data.url.toString())
-            txtRecieveMsg.text = data.msg
-
             if (data.is_image == true) {
-
-                imgLeft.visibility = View.VISIBLE
-                txtRecieveMsg.visibility = View.GONE
-            } else {
-                imgLeft.visibility = View.GONE
-                txtRecieveMsg.visibility = View.VISIBLE
+                imgLeft.LoadImg(data.url.toString())
             }
 
         }
 
     }
 
+    inner class MessageViewHolder3(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var txtSendMsg: TextView = itemView.findViewById(R.id.txtSendMsg)
+        fun bind(data: ChatMessages, position: Int) {
+            txtSendMsg.text = data.msg
+        }
+
+    }
+
+    inner class MessageViewHolder4(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var txtRecieveMsg: TextView = itemView.findViewById(R.id.txtRecieveMsg)
+
+        fun bind(data: ChatMessages, position: Int) {
+            txtRecieveMsg.text = data.msg
+        }
+
+    }
+
     override fun getItemViewType(position: Int): Int {
         val type: Int = if (array[position].sender == Appconstants.UUID) {
-            SENDER
+            if (array[position].is_image == true) {
+                SENDER_IMG
+            } else {
+                SENDER_MSG
+            }
         } else {
-            RECIEVER
+            if (array[position].is_image == true) {
+                RECIEVER_IMG
+            } else {
+                RECIEVER_MSG
+            }
         }
         return type
     }
